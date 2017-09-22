12e0fb613f166476bd3603a7ff238168228768a7
Daniel Lopez <flipappleam@live.com>
2017-09-22 01:56:49 -0700

    Delete api-keys.ts

--- src/app/api-keys.ts
+++
@@ -1,9 +0,0 @@
-export const proPublicaKey = 'Po3ugxfqUY6m3lrsqbGQy2ThvI4OaZPJuWyzZaY7';
-
-export const masterFirebaseConfig = {
-  apiKey: "AIzaSyALJIF52PvkrQ-zyUX6KOn9-6Wx-X8gTUo",
-  authDomain: "citizenvigilant-51400.firebaseapp.com",
-  databaseURL: "https://citizenvigilant-51400.firebaseio.com",
-  storageBucket: "",
-  messagingSenderId: "700324470151"
-}
